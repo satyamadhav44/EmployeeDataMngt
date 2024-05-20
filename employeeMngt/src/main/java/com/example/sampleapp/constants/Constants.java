9de44ad16f688ccdd99276708fc91636163e1e27
Satya Theegela <satyamadhav44@gmail.com>
2024-05-20 14:54:49 +0800

    optimised imports

--- employeeMngt/src/main/java/com/example/sampleapp/constants/Constants.java
+++ employeeMngt/src/main/java/com/example/sampleapp/constants/Constants.java
@@ -3,7 +3,7 @@ package com.example.sampleapp.constants;
 
 public class Constants {
 
-    public static final String ROOT="/rest";
-    public static final String POST_INF0 ="/employee";
-    public static final String GET_INF0 ="/employee/details";
+    public static final String ROOT = "/rest";
+    public static final String POST_INF0 = "/employee";
+    public static final String GET_INF0 = "/employee/details";
 }
--- employeeMngt/src/main/java/com/example/sampleapp/service/EmployeeDataService.java
+++ employeeMngt/src/main/java/com/example/sampleapp/service/EmployeeDataService.java
@@ -5,7 +5,6 @@ import com.example.sampleapp.dto.Employee;
 import com.example.sampleapp.repository.EmployeeRepo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
